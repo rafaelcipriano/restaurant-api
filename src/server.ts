fa145b61cf23a07e2479f4be727c452e79eaa528
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-02 16:46:00 -0300

    feat(middleware): introduce a middleware for error handling
    This middleware is intended to assist in handling errors that may occur during the consumption of
    this API.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { errorHandling } from "./middlewares/error-handling";
 
 const PORT = 3333
 
 const app = express()
 
 app.use(express.json())
+app.use(errorHandling)
 
 app.listen(PORT, () => console.log("Server is running on port " + PORT))
