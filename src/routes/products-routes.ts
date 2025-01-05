34d5df2d70c103b2457f96563b470497a072edd6
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-05 19:53:11 -0300

    feat(routes): add DELETE method to the products route
    Allow the user to delete a record in the database with the delete method.

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -7,5 +7,6 @@ const productsController = new ProductControler()
 productRoutes.get("/", productsController.index)
 productRoutes.post("/", productsController.create)
 productRoutes.put("/:id", productsController.update)
+productRoutes.delete("/:id", productsController.delete)
 
 export { productRoutes }
