a0d246471ce87c62bce5b43f999732069981e397
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-05 13:23:28 -0300

    feat(controllers): create a method to add a record to the database
    Allow creating a record in the database with some validations using zod and using generic typescript
    to perform the insertion.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+import { z } from "zod"
 
 class ProductControler {
   async index(
@@ -10,6 +12,27 @@ class ProductControler {
       return response.json({ message: "Ok" })
     } catch (error) { 
       
+    }
+  }
+
+  async create(
+    request: Request, 
+    response: Response, 
+    next: NextFunction
+  ) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "value must be greather than zero"})
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      await knex<ProductRepository>("products").insert({ name, price })
+
+      return response.status(201).json()
+    } catch (error) {
+      next(error)
     }
   }
 }
