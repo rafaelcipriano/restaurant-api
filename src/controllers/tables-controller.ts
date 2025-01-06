9a2ee6c93401860d723a1cf354286ea51f7118ca
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-05 22:13:10 -0300

    feat(controllers): create a class with an index method to get records from the tables table

---
+++ src/controllers/tables-controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+
+class TablesController {
+  async index(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const tables = await knex<TablesRepository>("tables").select().orderBy("table_number")
+      
+      return response.json({ tables })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+}
+
+export { TablesController }
