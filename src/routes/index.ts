a241b469cd35ad5965dd5a1f7d4e8c044b789819
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-06 21:28:10 -0300

    feat(routes): add route to handle status of tables from table-sessions table

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { productRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
 
 const routes = Router()
 
 routes.use("/products", productRoutes)
 routes.use("/tables", tablesRoutes)
+routes.use("/tables-sessions", tablesSessionsRoutes)
 
 export default routes
