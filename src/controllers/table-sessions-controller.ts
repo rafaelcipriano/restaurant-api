712ab29ea86e5f463445fbe0ba63de85f95340ab
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-07 22:17:08 -0300

    feat(controllers): add update method to update table_sessions record
    The update method will allow you to update the value of the closed_at record by passing a date and
    time.

--- src/controllers/table-sessions-controller.ts
+++ src/controllers/table-sessions-controller.ts
@@ -50,6 +50,40 @@ class TableSessionsController {
       next(error)
     }
   }
+
+  async update(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id)
+
+      const session = await knex<TablesSessionsRepository>("table_sessions")
+        .where({ id })
+        .first()
+
+      if(!session) {
+        throw new AppError("session table not found")
+      }
+
+      if(session.closed_at) {
+        throw new AppError("this session table is already closed")
+      }
+
+      await knex<TablesSessionsRepository>("table_sessions")
+        .update({ closed_at: knex.fn.now() })
+        .where({ id })
+
+      return response.json()
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 export { TableSessionsController }
