fcf5f54748a3ce3cfa0db87a3f2f39f3bcc821d5
rafaelcipriano <rafaelciprianosilva0@gmail.com>
2025-01-08 00:10:49 -0300

    feat(routes): add POST method and create method to order route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { productRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
 import { tablesSessionsRoutes } from "./table-sessions-routes";
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router()
 
 routes.use("/products", productRoutes)
 routes.use("/tables", tablesRoutes)
 routes.use("/tables-sessions", tablesSessionsRoutes)
+routes.use("/orders", ordersRoutes)
 
 export default routes
